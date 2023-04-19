35d41ddd22b251841f65fee8665b1be8e9092ffe
Aybi22 <your_email_address@example.com>
2023-04-19 15:05:38 +0100

    "new commit"

--- index.js
+++ index.js
@@ -87,3 +87,9 @@ containerElement.innerHTML=`thank you`;
 function myFunction() {
     document.getElementById("myForm").submit();
   }
+
+  let btn1=document.querySelector(".btn1");
+  btn1.addEventListener("click",()=>{
+    img7.style.display="block;"
+  });
+
