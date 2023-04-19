4158dd89a1404c18eb9738c4cf6e1042a3278e11
Aybi22 <your_email_address@example.com>
2023-04-19 07:26:02 +0100

    "new commit"

--- index.js
+++ index.js
@@ -2,7 +2,37 @@ const ratingElements=document.querySelectorAll(".rating");
 ratingElements.forEach((ratingElement) => {
 ratingElement.addEventListener("click", (event) =>{
     console.log(event.target.innerText|| event.target.parentNode.innerText);
-});
+    event.target.classList.add("active");
+    event.target.parentNode.classList.add("active");
+
+
 
 });
 
+});
+
+let selectedRating="";
+ratingElement.addEventListener("click", (event) =>{
+removeActive();
+
+event.target.innerText|| event.target.parentNode.innerText;
+
+});
+
+
+
+
+
+
+
+
+
+const btnElement=document.getElementById('btn');
+const containerElement=document.getElementById('container');
+btnElement.addEventListener('click',()=>{
+    if(selectedRating !=="") {
+containerElement.innerHTML=`thank you`;
+    
+}
+
+});
