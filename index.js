5101f34f608e07a01beaf749deb107086baf4016
Aybi22 <your_email_address@example.com>
2025-05-22 15:46:03 +0100

    "changing h1 font-size"

--- index.js
+++ index.js
@@ -3,115 +3,115 @@
 
 
 //Return the first element of the specified class
-  var sliderimg=document.querySelector('.slider-image');
-  
-  //variable to store the images
-  var images=['img1.jpg','img2.jpg','img3.jpg','img4.jpg','img5.jpg','img6.jpg','img7.jpg','img8.jpg','img9.jpg','img10.jpg',
+var sliderimg = document.querySelector('.slider-image');
+
+//variable to store the images
+var images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg', 'img7.jpg', 'img8.jpg', 'img9.jpg', 'img10.jpg',
   'img11.jpg'];
-  
-  //Current image index
-  var i=0;
+
+//Current image index
+var i = 0;
 
 //Create function for the previous button
 function prev() {
-  if (i <= 0) i= images.length; //Define condition to display the previous image
+  if (i <= 0) i = images.length; //Define condition to display the previous image
   i--;
   return showImg();//Declare a function to display images
-  
-  }
+
+}
 //Create function for the next button
 function next() {
-  if(i >= images.length -1) i=-1;//Define condition to display the next image
+  if (i >= images.length - 1) i = -1;//Define condition to display the next image
   i++;//Add 1 when image number is bigger than  image lenght
   return showImg();
-  
-  }
 
-		//Display images
-function showImg(){
+}
 
-  return sliderimg.setAttribute('src','images/'+ images[i]);//Add a class attribute to images
+//Display images
+function showImg() {
+
+  return sliderimg.setAttribute('src', 'images/' + images[i]);//Add a class attribute to images
 }
 
 
 
 
-const small=document.querySelector(".small");//Create a variable
+const small = document.querySelector(".small");//Create a variable
 small.addEventListener("click", function (changeColor) { //Set a function
-  document.querySelector(".small").style.backgroundColor="red";//Change the background color of the selected element
+  document.querySelector(".small").style.backgroundColor = "red";//Change the background color of the selected element
 });
 
 
- 
-const ratingElements=document.querySelectorAll(".rating");//Create a variable to Access all the rating elements
+
+const ratingElements = document.querySelectorAll(".rating");//Create a variable to Access all the rating elements
 
 ratingElements.forEach((ratingElement) => { //Access each of the rating elements
-ratingElement.addEventListener("click", (event) =>{ //Create an event when we click on a rating element
+  ratingElement.addEventListener("click", (event) => { //Create an event when we click on a rating element
     removeActive();//Function to remove all active elements
-    selectedRating=
-    event.target.innerText|| event.target.parentNode.innerText;//Create the value of the inner text when rating is selected
+    selectedRating =
+      event.target.innerText || event.target.parentNode.innerText;//Create the value of the inner text when rating is selected
     //Add active class to the html file
     event.target.classList.add("active");//Add active class to the text under the emoji
     event.target.parentNode.classList.add("active");//Add active class to the parent element(the emoji)
-});
+  });
 
 });
 
 //Remove all the active classes
-function removeActive(){
-ratingElements.forEach((ratingElement)=>{
+function removeActive() {
+  ratingElements.forEach((ratingElement) => {
     ratingElement.classList.remove("active");//Remove the active class for each rating element
 
-});
+  });
 }
 
 
 
 //Create a variable for selected ratings
-let selectedRating="";
+let selectedRating = "";
 
-const btnElement=document.querySelector(".btn");//Create a variable to select the button element
-const headingElement=document.querySelector(".heading");//Select the heading element
-btnElement.addEventListener("click",()=>{ //Create a function to display text when the button is clicked
-    if(selectedRating !=="") { //Create condition to trigger the function
+const btnElement = document.querySelector(".btn");//Create a variable to select the button element
+const headingElement = document.querySelector(".heading");//Select the heading element
+btnElement.addEventListener("click", () => { //Create a function to display text when the button is clicked
+  if (selectedRating !== "") { //Create condition to trigger the function
 
-//Change the HTML element
-headingElement.innerHTML=`Thank you!
- Feedback:${selectedRating} <br><br> 
+    //Change the HTML element
+    headingElement.innerHTML = `Thank you!
+ your Feedback is:"${selectedRating}" <br><br> 
 
 <p>you feedback is very important to us!</p>
 `
 
-}
+  }
 
 });
 
 
-let btn1=document.querySelector(".btn1"); 
-  btn1.addEventListener("click",function(showImage){ //create a function that displays the image when the button is clicked
-    document.querySelector(".pic").style.display="block";
-   
+let btn1 = document.querySelector(".btn1");
+btn1.addEventListener("click", function (showImage) { //create a function that displays the image when the button is clicked
+  document.querySelector(".pic").style.display = "block";
+
 
 });
 
 //Create variable
-let pictures=['images/img14.jpg', 'images/img15.webp' ,'images/img16.jpg','images/img17.jpg','images/img18.jpg','images/img19.jpg'];
-let n=0;//Set starting point
-let time=3000;
+let pictures = ['images/img14.jpg', 'images/img15.webp', 'images/img16.jpg', 'images/img17.jpg', 'images/img18.jpg', 'images/img19.jpg'];
+let n = 0;//Set starting point
+let time = 3000;
 //Create function to display images
-function displayImg(){
-  document.picture.src=pictures[n];
-  if(n<pictures.length -1){
+function displayImg() {
+  document.picture.src = pictures[n];
+  if (n < pictures.length - 1) {
     n++;// Add 1 to Index
-  }else{
-    n=0;
+  } else {
+    n = 0;
   }
   //Define function running time
   setTimeout("displayImg()", time);
 
 }
 // Run function when page loads
-window.onload=displayImg;
+window.onload = displayImg;
 
 
 
